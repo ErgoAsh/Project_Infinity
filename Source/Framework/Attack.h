90564971d8d6742824487733eca89930731249d1
Marek K <ErgoAsh@users.noreply.github.com>
2016-02-15 14:54:32 +0100

    Let's start a massive attack of android commits
    Commitsu - a sneak kills a deadman

--- Source/Framework/Attack.h
+++ Source/Framework/Attack.h
@@ -9,11 +9,23 @@
  * 
  */
 UCLASS()
-class FRAMEWORK_API UAttack : public UObject
-{
+class FRAMEWORK_API UAttack : public UObject {
+
 	GENERATED_BODY()
 	
-	
-	
-	
+	UAnimSequence* Animation;
+	FExecuteEvent ExecuteEvent;
+
+public:
+	UDodge();
+
+	UFUNCTION(BlueprintCallable, Category = "Skill")
+	UAnimationAsset* GetAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Skill")
+	void Execute(ABaseCharacter* Executor) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Skill")
+	TArray<TMulticastScriptDelegate*> GetEvents() override;
+
 };
